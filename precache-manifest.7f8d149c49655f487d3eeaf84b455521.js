09fa2678b6222c6cc6e6df8076ece57ec4039f63
ArctcShadow <57475574+ArctcShadow@users.noreply.github.com>
2023-05-12 21:00:32 +0300

    Updates

---
+++ precache-manifest.7f8d149c49655f487d3eeaf84b455521.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58ca4b4c19ef498d6e2415c7b6428305",
+    "url": "/diploma/index.html"
+  },
+  {
+    "revision": "44ef8be558128c194f21",
+    "url": "/diploma/static/css/2.5abeca67.chunk.css"
+  },
+  {
+    "revision": "042ab4dce20a6f23e7a0",
+    "url": "/diploma/static/css/main.f76061ee.chunk.css"
+  },
+  {
+    "revision": "44ef8be558128c194f21",
+    "url": "/diploma/static/js/2.4a2bf2a1.chunk.js"
+  },
+  {
+    "revision": "042ab4dce20a6f23e7a0",
+    "url": "/diploma/static/js/main.61e0fec6.chunk.js"
+  },
+  {
+    "revision": "f8ef87c39c8f6d123efe",
+    "url": "/diploma/static/js/runtime-main.26e46f07.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.347ff10be697b9ee1d8344e190e3ee66.js"
+  "/diploma/precache-manifest.7f8d149c49655f487d3eeaf84b455521.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/diploma/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
