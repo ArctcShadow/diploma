c1bb77017276e9d2428df26715feb2eab6942b37
ArctcShadow <57475574+ArctcShadow@users.noreply.github.com>
2023-05-12 12:59:55 +0300

    Updates

---
+++ precache-manifest.347ff10be697b9ee1d8344e190e3ee66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce09766291f82e91fc5430b7c8f33b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44ef8be558128c194f21",
+    "url": "/static/css/2.5abeca67.chunk.css"
+  },
+  {
+    "revision": "cc695a3b3884720def99",
+    "url": "/static/css/main.f76061ee.chunk.css"
+  },
+  {
+    "revision": "44ef8be558128c194f21",
+    "url": "/static/js/2.4a2bf2a1.chunk.js"
+  },
+  {
+    "revision": "cc695a3b3884720def99",
+    "url": "/static/js/main.b640d1f3.chunk.js"
+  },
+  {
+    "revision": "0dc0d38f66c1f4110a61",
+    "url": "/static/js/runtime-main.aca99000.js"
+  }
+]);
